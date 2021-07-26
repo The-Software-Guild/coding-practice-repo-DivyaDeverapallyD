aed7068381adc94707199b3b9844249bdeea0f7c
DivyaDeverapallyD <divya.deverapally@gmail.com>
2021-07-26 16:59:29 -0400

    practice programs

---
+++ PracticeProgramming/src/main/java/variables/BestAdderEver.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variables;
+
+/**
+ *
+ * @author SandeepReddy
+ */
+public class BestAdderEver {
+    
+    public static void main(String[] args)
+    {
+        int a=1, b=2,c=3;
+        int sum=a+b+c;
+        System.out.println(sum);
+        int z=10;
+    }
+    
+}
---
+++ PracticeProgramming/src/main/java/variables/MenuOfChampions.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variables;
+
+/**
+ *
+ * @author SandeepReddy
+ */
+public class MenuOfChampions {
+      public static void main(String[] args) {
+          System.out.println("oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo");
+           System.out.println();
+          System.out.println("Welcome to restaurent");
+           System.out.println("oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo");
+            System.out.println();
+              System.out.println("Today's Menu");
+             System.out.println("  $ 500.00 *** Slice of Big Rico Pizza");
+               System.out.println(" $ 2.00 ***** Invisible Strawberry Pie");
+                 System.out.println(" $ 1.50 ***** Denver Omelet");
+
+      }
+    
+}
